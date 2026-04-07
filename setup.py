7af1d1c0431b77867f51b1536c4e9151f0743519
Akshaj Thenua <73123449+thenameisakshaj@users.noreply.github.com>
2026-04-08 00:05:55 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='Generative AI Project',
+    version='0.0.0',
+    author='Akshaj Thenua',
+    author_email='akshajthenua@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)   
